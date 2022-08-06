fbe7256cf2e2ad6955e01d2833b10bd9be6417fa
jwhankins177 <jwhankins177@gmail.com>
2022-08-06 12:32:46 -0500

    Question 9

--- data/scripts.sql
+++ data/scripts.sql
@@ -6,32 +6,62 @@ FROM data_analyst_jobs;
 --Answer 1793
 
 /* 2.	Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row? */
+SELECT *
+FROM data_analyst_jobs
+LIMIT 1;
 
---Answer
+--Answer ExxonMobil
 
 /* 3.	How many postings are in Tennessee? How many are there in either Tennessee or Kentucky? */
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location='TN'
+OR location='KY';
 
---Answer
+--Answer TN = 21, KY = 6, both = 27
+
+/* Work check */
+SELECT *
+FROM data_analyst_jobs
+WHERE location='TN'
+OR location='KY';
 
 /* 4.	How many postings in Tennessee have a star rating above 4? */
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location='TN'
+AND star_rating>+4;
 
---Answer
+--Answer 3
 
 /* 5.	How many postings in the dataset have a review count between 500 and 1000? */
+SELECT COUNT(*)
+FROM data_analyst_jobs
+WHERE location='TN'
+AND star_rating>=4;
 
---Answer
+--Answer 4
 
 /* 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating? */
+SELECT location,AVG(star_rating) as avg_rating
+FROM data_analyst_jobs
+GROUP BY location
+ORDER BY avg_rating DESC;
 
---Answer
+--Answer NE
 
 /* 7.	Select unique job titles from the data_analyst_jobs table. How many are there? */
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs;
 
---Answer
+--Answer 881
 
 /* 8.	How many unique job titles are there for California companies? */
+SELECT COUNT(DISTINCT title)
+FROM data_analyst_jobs
+WHERE location='CA';
 
---Answer
+--Answer 230
 
 /* 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations? */
 
